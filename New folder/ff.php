5150d60202e3f2698e0f1d23f4a76c8d3bd47128
sonamcodesoftic <sonam.codesoftic@gmail.com>
2021-08-26 10:56:53 +0530

    main

---
+++ New folder/ff.php
@@ -0,0 +1,40 @@
+<!-- Navbar Starts-->
+<nav class="navbar navbar-expand-md ">
+           
+           <a class="navbar-brand" href="#">LOGO</a>  <!-- Navbar Brand -->
+           <!-- Toggler/collapsibe Button -->
+           <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#collapsibleNavbar">
+             <span class="navbar-toggler-icon"><i class="fas fa-bars"></i></span>
+           </button>
+           <!-- Navbar links -->
+           <!-- Navbar Items Start -->
+           <div class="collapse navbar-collapse justify-content-center" id="collapsibleNavbar">
+             <ul class="navbar-nav ">
+               <li class="nav-item dropdown">
+                   <a class="nav-link dropdown-toggle" data-toggle="dropdown" href="#">Features</a>
+                   <div class="dropdown-menu">
+                     <a class="dropdown-item" href="#">About</a>
+                     <a class="dropdown-item" href="#">Contact Us</a>
+                     <a class="dropdown-item" href="#">Updates | FAQ</a>
+                     <a class="dropdown-item" href="#">Signup</a>
+                   </div>
+                 </li>
+               <li class="nav-item">
+                 <a class="nav-link" href="#">About</a>
+               </li>
+               <li class="nav-item">   
+                 <a class="nav-link" href="#">Contact Us</a>
+               </li>
+               <li class="nav-item">   
+                   <a class="nav-link" href="#">Updates | FAQ</a>
+                 </li>
+                 <li class="nav-item">   
+                   <a class="nav-link" href="#">Signup</a>
+                 </li>
+             </ul>
+           </div>
+           <!-- Navbar items End -->
+           <button class='demo-button pt-1 pb-1 pr-3 pl-3'><a class="" href="#">Schedule  Demo</a></button> <!-- Navbar Button -->
+         </nav>
+         
+         <!-- Navbar Ends -->
